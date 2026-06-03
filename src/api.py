0a10722ac7ebe267663e9adf532f12fb7fa04a59
Aleksandr Andreev <dreevxq@gmail.com>
2026-06-03 15:49:39 +0300

    feat: add cors

--- src/api.py
+++ src/api.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 from src.service import update_all_data_and_get_new_file, insert_data
 
 app = FastAPI()
+
+# Define the origins allowed to access your API backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          
+    allow_credentials=False,      
+    allow_methods=["GET", "POST"],        
+    allow_headers=["*"],          
+)
 
 @app.get('/file')
 async def get_data_from_db():
